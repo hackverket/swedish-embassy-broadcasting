05841297e14fab951048c82ffd4c74d17f5d9b1a
Christian Svensson <blue@cmd.nu>
2017-08-06 00:10:41 +0200

    lel

--- server/public/js/load.js
+++ server/public/js/load.js
@@ -1,4 +1,14 @@
 $( document ).ready(function() {
+
+  $('#circle').circleProgress({
+    value: 0.0,
+    size: 80,
+    fill: {
+      gradient: ["red", "orange"]
+    }
+  });
+
+
 	getData();
 
 	setInterval(function(){ getData(); }, 3000);
@@ -10,6 +20,15 @@ function getData() {
 		$("#entry1-text").text("NOW PLAYING: " + data[0].Title);
 		$('.korv').html('');
 
+    if (data.length > 1) {
+      var past = data[0].Duration * -1;
+      var next = data[1].Duration;
+      var progress = past / (past + next);
+      $('.circle').circleProgress('value', progress); 
+    } else {
+      $('.circle').circleProgress('value', 0); 
+    }
+
 		_.forEach(data, function (value, key) {
 			if(key > 0 && key < 4) {
 				$('.korv').append(''+
@@ -19,6 +38,5 @@ function getData() {
         		'</div>')
 			}
 		});
-
 	});
 }
