ebcb5506271aa1182a08157eaed5ce486ef2f18a
Christian Svensson <blue@cmd.nu>
2017-08-05 17:07:22 +0200

    fix queue endpoint

--- server/api/module.go
+++ server/api/module.go
@@ -3,9 +3,12 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/hackverket/swedish-embassy-broadcasting/server/api/song"
+	"github.com/hackverket/swedish-embassy-broadcasting/server/api/queue"
 )
 
 func New(parent *gin.RouterGroup) {
 	songGroup := parent.Group("/song")
 	song.New(songGroup)
+	queueGroup := parent.Group("/queue")
+	queue.New(queueGroup)
 }
