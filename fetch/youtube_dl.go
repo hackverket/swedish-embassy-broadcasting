a86a4fe40bfe8e757b65cd8d75846c0a0c3dc0ad
coral <jonas@jonasbengtson.se>
2017-08-05 13:38:59 +0200

    indentation

--- fetch/youtube_dl.go
+++ fetch/youtube_dl.go
@@ -2,16 +2,15 @@ package fetch
 
 import (
 	"encoding/hex"
+	"os"
 	"os/exec"
-  "os"
-  "path"
-  "fmt"
+	"path"
 )
 
 type AudioOutput struct {
-	Path string
-	Info string
-  CmdOutput string
+	Path      string
+	Info      string
+	CmdOutput string
 }
 
 func DownloadAudio(id string) (AudioOutput, error) {
@@ -22,12 +21,12 @@ func DownloadAudio(id string) (AudioOutput, error) {
 		"--audio-format=mp3",
 		"--write-info-json",
 		"--max-filesize=30m",
-		"--output="+ prefix + ".%(ext)s",
+		"--output="+prefix+".%(ext)s",
 		id)
 	o, err := cmd.Output()
 	output := AudioOutput{
-    Path: prefix + ".mp3",
-    Info: prefix + ".info.json",
-    CmdOutput: o}
+		Path:      prefix + ".mp3",
+		Info:      prefix + ".info.json",
+		CmdOutput: o}
 	return output, err
 }
