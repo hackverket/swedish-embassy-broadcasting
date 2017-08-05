96d03b158ed69529fca2a1a5eae456675cb871d0
coral <jonas@jonasbengtson.se>
2017-08-05 22:14:34 +0200

    NO FUCKING IDEA

--- server/server.go
+++ server/server.go
@@ -1,9 +1,6 @@
 package server
 
 import (
-	"time"
-
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/hackverket/swedish-embassy-broadcasting/server/api"
@@ -14,14 +11,6 @@ func Start() {
 
 	sAPI := r.Group("/api")
 	api.New(sAPI)
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
 
 	r.Use(static.Serve("/", static.LocalFile("server/public", true)))
 
