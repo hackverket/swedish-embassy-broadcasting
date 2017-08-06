9624a1dec96be77ebb9768d619e12b510bb8b330
Christian Svensson <blue@cmd.nu>
2017-08-07 00:16:52 +0200

    ws

--- server/api/mixer/get.go
+++ server/api/mixer/get.go
@@ -2,9 +2,17 @@ package mixer
 
 import (
   "time"
+  "strconv"
+  "log"
 	"github.com/gin-gonic/gin"
   "github.com/hackverket/swedish-embassy-broadcasting/motuavb"
+	"github.com/gorilla/websocket"
 )
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 var (
   level []float64
@@ -22,5 +30,17 @@ func init() {
 }
 
 func getMixer(c *gin.Context) {
-	c.JSON(200, level)
+  w := c.Writer
+  r := c.Request
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+
+  for {
+    // Channel 22 is one of the output channels
+    conn.WriteMessage(websocket.TextMessage, []byte(strconv.FormatFloat(level[22], 'g', 1, 64)))
+    time.Sleep(10 * time.Millisecond)
+  }
 }
