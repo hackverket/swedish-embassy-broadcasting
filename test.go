0ea1eab50f122d9f11be179af019d07c08d6a006
coral <jonas@jonasbengtson.se>
2017-08-06 14:18:20 +0200

    sauna timer

--- test.go
+++ test.go
@@ -3,9 +3,9 @@ package main
 import (
 	"sync"
 
-	"github.com/hackverket/swedish-embassy-broadcasting/sauna"
 	"github.com/hackverket/swedish-embassy-broadcasting/server"
 	"github.com/hackverket/swedish-embassy-broadcasting/shitirc"
+	"github.com/hackverket/swedish-embassy-broadcasting/timer"
 )
 
 func main() {
@@ -18,7 +18,7 @@ func main() {
 	sirc := shitirc.Dial("#sha2017", "irc.quakenet.org:6667", "HACK4JESUS")
 	go sirc.Connect()
 	go server.Start()
-	go sauna.Start()
+	go timer.Start()
 
 	wg.Wait()
 }
