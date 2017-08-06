ffe423d04fb1709d3e75fbf7a625fbfb5da87664
Christian Svensson <blue@cmd.nu>
2017-08-06 22:34:48 +0200

    proxy

--- server/api/mixer/get.go
+++ server/api/mixer/get.go
@@ -1,11 +1,25 @@
 package mixer
 
 import (
+  "time"
 	"github.com/gin-gonic/gin"
   "github.com/hackverket/swedish-embassy-broadcasting/motuavb"
 )
 
+var (
+  level []float64
+)
+
+func init() {
+  go func() {
+    for {
+      sc := motuavb.Connect("10.44.22.107")
+      level = sc.GetMeters("mix/level/0")
+      time.Sleep(10 * time.Millisecond)
+    }
+  }()
+}
+
 func getMixer(c *gin.Context) {
-  sc := motuavb.Connect("10.44.22.107")
-	c.JSON(200, sc.GetMeters("mix/level/0"))
+	c.JSON(200, level)
 }
