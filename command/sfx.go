01b77179204507faf79e288e549b2bfcf94d5ee5
coral <jonas@jonasbengtson.se>
2017-08-06 12:36:39 +0200

    SOUNDS

--- command/sfx.go
+++ command/sfx.go
@@ -1,11 +1,14 @@
 package command
 
 import (
+	"fmt"
 	"os/exec"
 )
 
 func Sfx(sfx string) {
-	wavpath := "/home/bluecmd/go/src/github.com/hackverket/swedish-embassy-broadcasting/" + sfx + ".wav"
+
+	fmt.Println(sfx)
+	wavpath := "/home/bluecmd/go/src/github.com/hackverket/swedish-embassy-broadcasting/sounds/" + sfx + ".wav"
 
 	paplayArgs := append([]string{
 		"-s", "127.0.0.1",
@@ -13,5 +16,7 @@ func Sfx(sfx string) {
 		wavpath,
 	})
 
-	exec.Command("paplay", paplayArgs...)
+	cmd := exec.Command("paplay", paplayArgs...)
+	o, err := cmd.Output()
+	fmt.Println(o, err)
 }
