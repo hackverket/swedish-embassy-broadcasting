de594170519c7046fa510e401301d83aff3b3f49
coral <jonas@jonasbengtson.se>
2017-08-07 18:40:49 +0200

    test

--- server/api/effect/post_playeffect.go
+++ server/api/effect/post_playeffect.go
@@ -1,6 +1,8 @@
 package effect
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hackverket/swedish-embassy-broadcasting/command"
 )
@@ -13,6 +15,7 @@ func playEffect(c *gin.Context) {
 	var json effectRequest
 	if c.BindJSON(&json) == nil {
 
+		fmt.Println(json.effectName)
 		go command.Sfx(json.effectName)
 		c.JSON(
 			200,
